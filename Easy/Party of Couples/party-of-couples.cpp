8b87505e70755dee51cde29ed07ef1ade0f828b6
JaiDixit07 <157274687+JaiDixit07@users.noreply.github.com>
2024-04-10 20:04:43 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Party of Couples/party-of-couples.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    int findSingle(int n, int arr[]){
+        // code here
+         sort(arr, arr+n );
+        int ans = -1 ;
+        int cnt= 1;
+        for( int i =0 ; i < n ; i++){
+            if (arr[i]==arr[i+1]) {
+            cnt++;
+            }
+            else {
+                if(cnt % 2 != 0) {
+                ans = arr[i];
+                return ans ;
+                }
+                else {
+                    cnt= 1 ;
+                }
+                
+            }
+        }
+        return ans ;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N, X;
+        cin >> N;
+        int arr[N];
+        for(int i = 0; i < N; i++){
+            cin >> arr[i];
+        }
+        
+        Solution ob;
+        cout << ob.findSingle(N, arr) << endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
