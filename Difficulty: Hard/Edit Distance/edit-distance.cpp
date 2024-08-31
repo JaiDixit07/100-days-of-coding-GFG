712a8c15bfa9403cb145fa2a8f5b0d43cadbb220
Jai Dixit <157274687+JaiDixit07@users.noreply.github.com>
2024-08-31 09:55:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Edit Distance/edit-distance.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int editDistance(string s1, string s2) {
+        // Code here
+        int n = s1.size(), m = s2.size();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= m; j++) {
+                if (i == 0)
+                    dp[i][j] = j; // s1 is empty, insert all characters of s2
+                else if (j == 0)
+                    dp[i][j] = i; // s2 is empty, remove all characters of s1
+                else if (s1[i - 1] == s2[j - 1])
+                    dp[i][j] = dp[i - 1][j - 1]; // Characters match, no operation needed
+                else
+                    dp[i][j] = 1 + min({dp[i - 1][j],     // Remove
+                                        dp[i][j - 1],     // Insert
+                                        dp[i - 1][j - 1]}); // Replace
+            }
+        }
+        return dp[n][m];
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        string s, t;
+        cin >> s >> t;
+        Solution ob;
+        int ans = ob.editDistance(s, t);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
